2894933e45e62f176587a3bbdbba3fa9fd5f06e5
rido <rmpablos@microsoft.com>
2025-02-11 07:16:18 -0800

    Update @microsoft/agents-bot-hosting to version 0.1.27 and refactor bot.js to include package version in greeting

--- src/bot.js
+++ src/bot.js
@@ -1,23 +1,22 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler, MessageFactory, ActionTypes } = require('@microsoft/agents-bot-hosting');
+const { ActivityHandler, MessageFactory } = require('@microsoft/agents-bot-hosting');
+const {name, version} = require('@microsoft/agents-bot-hosting/package.json');
 
 class EchoBot extends ActivityHandler {
     constructor() {
         super();
 
         this.onMembersAdded(async (context, next) => {
-            await context.sendActivity(MessageFactory.text('Hello and welcome!'));
-
-            // By calling next() you ensure that the next BotHandler is run.
+            await context.sendActivity(MessageFactory.text(`EchoBot running on ${name} version ${version}`));
             await next();
         });
 
         this.onMessage(async (context, next) => {
             const text = context.activity.text;
 
-            context.sendActivities([
+            await context.sendActivities([
                 MessageFactory.text(`You said: ${ text }`, text),
                 MessageFactory.text(`echo: ${ text }`, text),
             ])
